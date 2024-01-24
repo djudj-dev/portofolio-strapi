231fc3953c1a8d81f51e25f1b5554dd56f7ee074
= <vannierjulian@protonmail.com>
2024-01-24 17:15:01 +0100

    fixes #1, fixes #2, fixes #3, fixes #6; add strapi cms and create needed collections

---
+++ src/api/tech/controllers/tech.ts
@@ -0,0 +1,7 @@
+/**
+ * tech controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::tech.tech');
---
+++ src/api/tech/services/tech.ts
@@ -0,0 +1,7 @@
+/**
+ * tech service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::tech.tech');
